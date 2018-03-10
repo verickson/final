418f4184192450a51b4264f4252e79527c48a69b
Victoria Erickson <victoriamerickson@gmail.com>
2018-03-09 22:06:43 -0500

    more packages and moved out style to a new file

---
+++ src/components/css/styles.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container:{
+    backgroundColor: '#ff00ff'
+  },
+  image:{
+    height: 50,
+    width: 50,
+  }
+});
+
+export default styles;
